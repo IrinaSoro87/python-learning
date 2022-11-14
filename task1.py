9af24596f865d6d70a51b7ea66294e157b6e68b7
IrinaSoro <Irinka-soro@mail.ru>
2022-11-14 11:24:00 +0300

    first commit

---
+++ task1.py
@@ -0,0 +1,22 @@
+with open("count_the_words") as file:
+   data = file.read()
+
+def unique_word_count(str):
+    uniqueWords = dict()
+    text = str.split()
+    
+    for word in text:
+        if word in uniqueWords:
+            uniqueWords[word]+=1
+        else:
+            uniqueWords[word]=1    
+            
+    return uniqueWords
+
+output = unique_word_count(data)
+
+print('Total number of unique words in the file is :', len(output))
+
+for key, value in sorted(output.items()):
+    print(key.capitalize(), ' ', value)
+    
